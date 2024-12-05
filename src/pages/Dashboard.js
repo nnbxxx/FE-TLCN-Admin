45d4ef1395645b16eee284a0e70acc5f4dbae846
nnbxxx <uyenbao4a5@gmail.com>
2024-12-05 13:59:24 +0700

    fix chart dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,91 +1,117 @@
 import React, { useEffect, useState } from "react";
 import { BsArrowDownRight, BsArrowUpRight } from "react-icons/bs";
 import { Column } from "@ant-design/plots";
-import { Table } from "antd";
+import { DatePicker, Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getMonthlyData,
   getOrders,
   getYearlyData,
 } from "../features/auth/authSlice";
-import { getInforDashBoard } from "../utils/api";
+import { getDataRevenue, getInforDashBoard } from "../utils/api";
 import { Link } from "react-router-dom";
-const data = [
-  { type: "product1", value: 0.16 },
-  { type: "product2", value: 0.125 },
-  { type: "product3", value: 0.24 },
-  { type: "product4", value: 0.19 },
-  { type: "product5", value: 0.22 },
-  { type: "product6", value: 0.05 },
-  { type: "product7", value: 0 },
-  { type: "produc8", value: 0.015 },
-  { type: "product9", value: 0.22 },
-  { type: "product10", value: 0.05 },
-  { type: "product11", value: 0 },
-  { type: "produc12", value: 0.015 },
-];
-const configx = {
-  data,
-  xField: "type",
-  yField: "value",
-  // color: "#FF5733",
+import moment from "moment/moment";
 
-  label: {
-    text: (originData) => {
-      return originData.value;
-    },
-    offset: 10,
-    textBaseline: "bottom",
-  },
-  shapeField: "hollow",
-  colorField: "type",
-  legend: {
-    color: { size: 72, autoWrap: true, maxRows: 3, cols: 4 },
-  },
-  axis: {
-    y: {
-      // tick: false,
-      // title: false,
-    },
-    x: {
-      grid: false,
-      tick: false,
-      label: false,
-      // title: false,
-    },
-  },
-};
 const Dashboard = () => {
   const dispatch = useDispatch();
+  const [year, setYear] = useState(2024);
+  const onChange = (date, dateString) => {
+    setYear(+dateString);
+  };
+  const [configTopSeller, setConfigTopSeller] = useState({
+    data: [],
+    xField: "name",
+    yField: "totalQuantityBought",
 
-  const [dataInitDashBorad, setDataInitDashBorad] = useState();
+    label: {
+      text: (originData) => {
+        return originData.totalQuantityBought;
+      },
+      offset: 10,
+      textBaseline: "bottom",
+    },
+    shapeField: "hollow",
+    colorField: "name",
+    legend: {
+      color: { size: 72, autoWrap: true, maxRows: 3, cols: 4 },
+    },
+    axis: {
+      y: {
+        // tick: false,
+        // title: false,
+      },
+      x: {
+        grid: false,
+        tick: false,
+        label: false,
+        // title: false,
+      },
+    },
+  });
+  const [configRevenue, setConfigRevenue] = useState({
+    data: [],
+    xField: "month",
+    yField: "value",
+
+    label: {
+      text: (originData) => {
+        return originData.value;
+      },
+      offset: 10,
+      textBaseline: "bottom",
+    },
+    shapeField: "hollow",
+    colorField: "month",
+    legend: {
+      color: { size: 72, autoWrap: true, maxRows: 3, cols: 4 },
+    },
+    axis: {
+      y: {
+        // tick: false,
+        // title: false,
+      },
+      x: {
+        grid: false,
+        tick: false,
+        label: false,
+        // title: false,
+      },
+    },
+  });
+  const [dataInitDashBoard, setDataInitDashBoard] = useState();
   useEffect(() => {
     dispatch(getOrders("current=1&pageSize=10&sort=-createdAt"));
   }, []);
 
-  const colors = [
-    "#ffd333",
-    "#ff6347",
-    "#4682b4",
-    "#32cd32",
-    "#ffa07a",
-    "#6a5acd",
-    "#ff69b4",
-    "#20b2aa",
-    "#ffb6c1",
-    "#87cefa",
-  ];
-
   const getInfoDashBoard = async () => {
     const re = await getInforDashBoard();
     if (re && re.data) {
-      setDataInitDashBorad(re.data);
+      setDataInitDashBoard(re.data);
+      setConfigTopSeller({
+        ...configTopSeller,
+        data: re.data.dataTopSellingProducts,
+      });
     }
   };
   useEffect(() => {
     getInfoDashBoard();
     return () => {};
   }, []);
+  const callGetDataRevenue = async () => {
+    const reDataRevenue = await getDataRevenue({
+      year: year,
+    });
+    if (reDataRevenue && reDataRevenue.data) {
+      setConfigRevenue({
+        ...configRevenue,
+        data: reDataRevenue.data,
+      });
+    }
+  };
+  useEffect(() => {
+    callGetDataRevenue();
+    return () => {};
+  }, [year]);
   const orderState = useSelector((state) => state?.auth?.orders);
   const columns = [
     {
@@ -139,8 +165,8 @@ const Dashboard = () => {
           <div>
             <p className="desc">Tổng doanh thu Sắc</p>
             <h4 className="mb-0 sub-title">
-              {dataInitDashBorad && dataInitDashBorad?.totalRevenue
-                ? dataInitDashBorad?.totalRevenue.toLocaleString("vi-VN")
+              {dataInitDashBoard && dataInitDashBoard?.totalRevenue
+                ? dataInitDashBoard?.totalRevenue.toLocaleString("vi-VN")
                 : 0}
               ₫
             </h4>
@@ -150,7 +176,7 @@ const Dashboard = () => {
           <div>
             <p className="desc">Số lượng sản phẩm đã bán</p>
             <h4 className="mb-0 sub-title">
-              {dataInitDashBorad && dataInitDashBorad?.totalProductPruchased}
+              {dataInitDashBoard && dataInitDashBoard?.totalProductPruchased}
             </h4>
           </div>
         </div>
@@ -158,7 +184,7 @@ const Dashboard = () => {
           <div>
             <p className="desc">Số lượng người dùng</p>
             <h4 className="mb-0 sub-title">
-              {dataInitDashBorad && dataInitDashBorad?.quantityUser}
+              {dataInitDashBoard && dataInitDashBoard?.quantityUser}
             </h4>
           </div>
         </div>
@@ -166,7 +192,7 @@ const Dashboard = () => {
           <div>
             <p className="desc">Số lượng bài viết</p>
             <h4 className="mb-0 sub-title">
-              {dataInitDashBorad && dataInitDashBorad?.quantityBlog}
+              {dataInitDashBoard && dataInitDashBoard?.quantityBlog}
             </h4>
           </div>
         </div>
@@ -174,26 +200,29 @@ const Dashboard = () => {
           <div>
             <p className="desc">Số lượng thương hiệu</p>
             <h4 className="mb-0 sub-title">
-              {dataInitDashBorad && dataInitDashBorad?.quantityBlog}
+              {dataInitDashBoard && dataInitDashBoard?.quantityBrand}
             </h4>
           </div>
         </div>
       </div>
       <div className="d-flex justify-content-between align-items gap-3">
         <div className="mt-4 flex-grow-1 w-50">
-          <h3 className="mb-5 title">Thu nhập </h3>
+          <h3 className="mb-5 title">10 sản phẩm bán nhiều nhất</h3>
           <div>
-            <Column {...configx} />
+            <Column {...configTopSeller} />
           </div>
         </div>
-        
       </div>
       <div className="d-flex justify-content-between align-items gap-3">
-        
         <div className="mt-4 flex-grow-1 ">
           <h3 className="mb-5 title">Doanh số </h3>
+          <DatePicker
+            onChange={onChange}
+            picker="year"
+            defaultValue={moment(year.toString(), "YYYY")}
+          />
           <div>
-            <Column {...configx} />
+            <Column {...configRevenue} />
           </div>
         </div>
       </div>
@@ -208,4 +237,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,6 @@ import instance from "./axios-customize";
 export const getInforDashBoard = () => {
   return instance.get(`dashboard/info`);
 };
+export const getDataRevenue = (data) => {
+  return instance.post(`dashboard/revenue`, data);
+};
